4488aff0c943cf802143a0ab56d390d35ba98214
Jill Bender <jbend@microsoft.com>
2018-07-09 16:01:09 -0700

    add constants file for common strings

--- Telemetry/RulesTest.cs
+++ Telemetry/RulesTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 using System.Net;
+using Helpers;
 using Helpers.Http;
 using Xunit;
 
@@ -9,7 +10,6 @@ namespace Telemetry
     public class RulesTest
     {
         private readonly IHttpClient httpClient;
-        private const string TELEMETRY_ADDRESS = "http://127.0.0.1:9004/v1";
 
         public RulesTest()
         {
@@ -20,11 +20,11 @@ namespace Telemetry
         /// Integration test using a real HTTP instance.
         /// Test that the service starts normally and returns ok status
         /// </summary>
-        [Fact, Trait("Type", "IntegrationTest")]
+        [Fact, Trait(Constants.TEST, Constants.INTEGRATION_TEST)]
         public void Should_Return_OK_Status()
         {
             // Act
-            var request = new HttpRequest(TELEMETRY_ADDRESS + "/status");
+            var request = new HttpRequest(Constants.TELEMETRY_ADDRESS + "/status");
             request.AddHeader("X-Foo", "Bar");
             var response = this.httpClient.GetAsync(request).Result;
 
