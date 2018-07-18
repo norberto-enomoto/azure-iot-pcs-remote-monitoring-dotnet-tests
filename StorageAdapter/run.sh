16d84dafef073bd9b32bb9359e1a5e09899405fd
Jill Bender <jbend@microsoft.com>
2018-07-17 18:28:10 -0700

    Re-add rules tests to merge with master.

--- StorageAdapter/run.sh
+++ StorageAdapter/run.sh
@@ -3,7 +3,7 @@
 # Note: Windows Bash doesn't support shebang extra params
 set -e
 
-TEST_SUITE="Storage Adapter tests"
+TEST_SUITE=$1
 
 TEST_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 APP_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/"
@@ -40,58 +40,14 @@ run_tests() {
     find . -name *.csproj | xargs dotnet test --configuration Release
 }
 
-#Script options  
-repo=""
-tag="testing"
-
-set_up() {
-    export DOCKER_TAG=$tag
-    check_dependency_docker
-    check_dependency_dotnet
-}
-
-run() {
-    start_containers
-    run_tests
-    stop_containers
-}
-
-tear_down() {
-    unset DOCKER_TAG
-}
-
-#### Parse script arguments
-while [[ $# -gt 0 ]] ;
-do
-    opt=$1;
-    shift;
-    case $opt in
-        -t|--tag) tag=$1; shift;;
-        -re|--repo) repo=$1; shift;;
-        *)  shift;;
-    esac
-done
-
 header "Running $TEST_SUITE"
 
-#### Set Up Tests
-set_up
+check_dependency_docker
+check_dependency_dotnet
 
-#### Run tests based on repo
-if [ "$repo" == "both" ] || [ "$repo" == "" ]; then
-    export REPO=dotnet
-    run
-    export REPO=java
-    run
-elif [[ "$repo" == "dotnet" ]]; then
-    export REPO=dotnet
-    run
-elif [[ "$repo" == "java" ]]; then
-    export REPO=java
-    run
-fi
+start_containers
 
-#### Tear down test
-tear_down
+run_tests
+stop_containers
 
 set +e
