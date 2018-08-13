eec51af527d285a3c0b511406a3218ff6c45be30
Elvin Morales <elmoral@microsoft.com>
2018-08-13 15:06:49 -0700

    Resync PackageApiModel

--- Config/Models/PackageApiModel.cs
+++ Config/Models/PackageApiModel.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
@@ -7,17 +8,20 @@ namespace Config
 {
     public class PackageApiModel
     {
+        private const string DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:sszzz";
+
         [JsonProperty("Id")]
         public string Id { get; set; }
 
         [JsonProperty("Name")]
         public string Name { get; set; }
 
+        [JsonProperty("Type")]
         [JsonConverter(typeof(StringEnumConverter))]
         public PackageType Type { get; set; }
 
-        [JsonProperty("Content")]
-        public string Content { get; set; }
+        [JsonProperty(PropertyName = "DateCreated")]
+        public string DateCreated { get; set; } = DateTimeOffset.UtcNow.ToString(DATE_FORMAT);
 
     }
 
