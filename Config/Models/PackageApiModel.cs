15107a371ac30754cc02b7bb1c4c6898a70b540b
Elvin Morales <elmoral@microsoft.com>
2018-08-13 10:09:49 -0700

    Integration tests for packages controller

---
+++ Config/Models/PackageApiModel.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Config
+{
+    public class PackageApiModel
+    {
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public PackageType Type { get; set; }
+
+        [JsonProperty("Content")]
+        public string Content { get; set; }
+
+    }
+
+    public enum PackageType
+    {
+        EDGE_MANIFEST
+    }
+}
